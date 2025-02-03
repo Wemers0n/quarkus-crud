54de2bc4977f2a1673b85b84c94a9e08d134ebd1
Wemerson <nerdhits001@gmail.com>
2025-02-03 14:26:44 -0300

    feat: implemented search by id and paged search

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.example.controller;
 
+import java.util.UUID;
+
 import com.example.model.User;
 import com.example.services.UserService;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -19,6 +25,21 @@ public class UserController {
 
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response find(@PathParam("id") UUID id){
+        return Response.ok(service.findUser(id)).build();
+    }
+
+    @GET
+    @Path("/all")
+    public Response findAll(@QueryParam("page") @DefaultValue("0") Integer page, 
+                            @QueryParam("pageSize") @DefaultValue("10") Integer pageSize){
+        
+        var users = this.service.findAll(page, pageSize);
+        return Response.ok(users).build();
     }
 
     @POST
--- src/main/java/com/example/services/UserService.java
+++ src/main/java/com/example/services/UserService.java
@@ -1,5 +1,8 @@
 package com.example.services;
 
+import java.util.List;
+import java.util.UUID;
+
 import com.example.model.User;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -12,5 +15,13 @@ public class UserService {
     public User save(User user){
         User.persist(user);
         return user;
+    }
+
+    public User findUser(UUID id){
+        return User.findById(id);
+    }
+
+    public List<User> findAll(Integer page, Integer pageSize){
+        return User.findAll().page(page, pageSize).list();
     }
 }
